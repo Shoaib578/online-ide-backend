2951c9f59b6386c3799cfe1bab4df7bd0fa9a6d6
Shoaib578 <theshoaibihsan10@gmail.com>
2022-09-29 16:32:53 +0500

    completed

--- routes/apis/code.js
+++ routes/apis/code.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { generateFile } = require("../../generate_files");
-const { spawn, fork, spawnSync,exec,execFile} = require("child_process");
+const { spawn, fork, spawnSync,exec,execFile} = require("node:child_process");
 var fs = require('fs');
 const path = require("path");
 
@@ -30,7 +30,7 @@ router.get("/run_code",(req,res)=>{
 
     var error= []
     var output =[]
-   
+    
     let filepath = req.query.code_file
     let language = req.query.language
     let code;
@@ -40,17 +40,61 @@ router.get("/run_code",(req,res)=>{
         code = req.query.code
     }
 
-   
     if(language == "bash"){
         execute = exec(code)
         console.log("bash")
+
+    }else if(language == "gcc"){
+            var gcc_filepath=filepath.toString().split("/")[2].split(".")[0]
+            console.log(gcc_filepath)
+       
+        exec(`gcc public/code_files/${gcc_filepath.concat('.c')} -o public/code_files/${gcc_filepath.concat(".exe")}`, (er, std, stder) => {
+            
+            if(stder){
+                error.push(stder)
+               
+            }
+           
+            
+            
+    
+        }).on('close', (code) => {
+           if(code == 1){
+            res.send({
+                "output":[],
+                "error":error
+            })
+           }else{
+        exec(gcc_filepath, {cwd: 'public\\code_files\\'},(err, stdout, stderr) => {
+                
+                output.push(stdout.toString())
+               
+                error.push(stderr.toString())
+              
+            
+            }).on('close', (code) => {
+                console.log(code)
+               
+                res.send({
+                    "output":output,
+                    "error":error
+                })
+             });
+           }
+           
+           
+         });;
+        
+       
+
 
     }else{
     execute = spawn(language,[filepath])
 
     }
 
-   
+    if(language != "gcc"){
+
 
     execute.stderr.on('data', function(data) {
        console.log(data.toString())
@@ -78,7 +122,7 @@ router.get("/run_code",(req,res)=>{
         "error":error
     })
  });
-
+}
 
 
    
